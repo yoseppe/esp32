c9ca6a1a20833a670935c09d276bcbb11d83da48
Josip Grgic <josip.grgic@fer.hr>
2023-09-22 14:10:57 +0200

    Cleaned up a little bit

--- station_copy2/components/display/oled_display.c
+++ station_copy2/components/display/oled_display.c
@@ -20,6 +20,8 @@
 SSD1306_t dev;
 uint8_t buff[1024];
 
+void spyrosoftLogo_scrollDown(void);
+
 void oled_init(void)
 {
 	i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
@@ -41,65 +43,68 @@ void oled_init(void)
 		// 	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo19, 128, 64, false);
 		// 	vTaskDelay(100 / portTICK_PERIOD_MS);
 		// }
-		
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_6, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_5, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_4, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_3, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_2, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_1, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo1, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo2, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo3, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo4, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo5, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo6, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo7, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo8, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo9, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo10, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo11, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo12, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo13, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo14, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo15, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo16, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo17, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo18, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo19, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo20, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo21, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo22, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_spyroLogoAllWhite, 128, 64, false);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
-		ssd1306_bitmaps(&dev, 0, 0, image_printingTimeInESPLOGI, 128, 64, false);
+	spyrosoftLogo_scrollDown();
+	ssd1306_bitmaps(&dev, 0, 0, image_printingTimeInESPLOGI, 128, 64, false);
 	//}
+}
+
+void spyrosoftLogo_scrollDown(void) {
+ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_6, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_5, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_4, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_3, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_2, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo_1, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo1, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo2, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo3, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo4, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo5, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo6, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo7, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo8, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo9, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo10, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo11, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo12, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo13, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo14, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo15, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo16, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo17, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo18, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo19, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo20, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo21, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogo22, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
+	ssd1306_bitmaps(&dev, 0, 0, image_spyroLogoAllWhite, 128, 64, false);
+	vTaskDelay(10 / portTICK_PERIOD_MS);
 }
